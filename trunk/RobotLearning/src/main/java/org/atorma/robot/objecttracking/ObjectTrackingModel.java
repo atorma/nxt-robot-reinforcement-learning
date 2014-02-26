c7b5f2c0769ccf234266ecf515255bf7fb79e55f
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-02-26 19:58:54 +0000

    Implementation of TrackedObject, but some tests still failing. Accumulation of computation of inaccuracies?

---
+++ trunk/RobotLearning/src/main/java/org/atorma/robot/objecttracking/ObjectTrackingModel.java
@@ -0,0 +1,20 @@
+package org.atorma.robot.objecttracking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ObjectTrackingModel {
+	
+	private List<TrackedObject> objects = new ArrayList<>();
+
+	public void addObservation(double distanceCm, double angleDegrees) {
+		TrackedObject object = new TrackedObject(distanceCm, angleDegrees);
+		objects.add(object);
+	}
+
+	public List<TrackedObject> getObjectLocations() {
+		return Collections.unmodifiableList(objects);
+	}
+
+}
