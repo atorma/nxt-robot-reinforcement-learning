8467aa02501f835bd6a1c1b830de8232b6a8c7a2
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-03-02 14:28:31 +0000

    Implemented CsvLogWriter

---
+++ RobotLearning/src/test/java/org/atorma/robot/logging/CsvLogWriterTests.java
@@ -0,0 +1,70 @@
+package org.atorma.robot.logging;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.*;
+
+import au.com.bytecode.opencsv.CSV;
+import au.com.bytecode.opencsv.CSVReadProc;
+
+public class CsvLogWriterTests {
+
+	private String[] columnHeaders = new String[] {"Column 1", "Column 2"};
+	
+	private CsvLogWriter logWriter;
+	
+	
+	@After
+	public void tearDown() {
+		logWriter.getOutputFile().delete();
+	}
+	
+	@Test
+	public void row_written_some_time_after_adding_and_can_be_read_back() throws InterruptedException {
+		logWriter = new CsvLogWriter(new File("test 1.csv"), columnHeaders);
+		
+		Object[] values = new String[] {"value 1", "value 2"};
+		logWriter.addRow(values);
+		Thread.sleep(100);
+		
+		CSV csv = logWriter.getCsvConfiguration();
+		
+		TestCSVReadProc readProc = new TestCSVReadProc();
+		csv.read(logWriter.getOutputFile(), readProc);
+		
+		assertEquals(2, readProc.rows.size());
+		assertArrayEquals(columnHeaders, readProc.rows.get(0));
+		assertArrayEquals(values, readProc.rows.get(1));
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void exception_if_row_contains_different_number_of_cells_than_headers() {
+		logWriter = new CsvLogWriter(new File("test 2.csv"), columnHeaders);
+		logWriter.addRow("value 1", "value 2", "value 3");
+	}
+	
+	@Test
+	public void null_does_not_cause_exception() {
+		logWriter = new CsvLogWriter(new File("test 3.csv"), columnHeaders);
+		
+		Object[] values = new String[] {null, "value 2"};
+		logWriter.addRow(values);
+	}
+	
+	
+	private class TestCSVReadProc implements CSVReadProc {
+		
+		private List<String[]> rows = new ArrayList<String[]>();
+
+		@Override
+		public void procRow(int rowIndex, String... values) {
+			rows.add(rowIndex, values);
+		}
+		
+	}
+}
