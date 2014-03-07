7b118b0936062d518d55df3965c202660db1201d
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-03-07 06:51:20 +0000

    Javadoc

--- trunk/RobotCore/src/main/java/org/atorma/robot/DiscreteRobotController.java
+++ trunk/RobotCore/src/main/java/org/atorma/robot/DiscreteRobotController.java
@@ -2,5 +2,9 @@ package org.atorma.robot;
 
 public interface DiscreteRobotController {
 
+	/**
+	 * Method invoked by a robot when it requires an action from the controller.
+	 * Learning can be hooked into this method.
+	 */
 	int getActionId(double[] stateValues);
 }
