ae5c32538210ea9133b2784855b45020abb06953
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-03-08 12:52:50 +0000

    Fixed bug in EqualWidthDiscretizer when value was equal to maximum of the range

--- RobotLearning/src/main/java/org/atorma/robot/discretization/EqualWidthDiscretizer.java
+++ RobotLearning/src/main/java/org/atorma/robot/discretization/EqualWidthDiscretizer.java
@@ -1,5 +1,10 @@
 package org.atorma.robot.discretization;
 
+/**
+ * Discretization where bin widths are equal and their ordinals are
+ * 0 = [min, min+binWidth), 1 = [min+binWidth, min+2*binWidth), ..., nBins-1: [min+(nBins-1)*binWidth, min+nBins*binWidth).
+ * Values smaller than the minimum are discretized to 0, larger than maximum to nBins-1.
+ */
 public class EqualWidthDiscretizer implements Discretizer {
 
 	private final double min;
@@ -26,9 +31,9 @@ public class EqualWidthDiscretizer implements Discretizer {
 	
 	@Override
 	public int discretize(double value) {
-		if (value < min) {
+		if (value <= min) {
 			return 0;
-		} else if (value > max) {
+		} else if (value >= max) {
 			return nBins - 1;
 		}
 		
