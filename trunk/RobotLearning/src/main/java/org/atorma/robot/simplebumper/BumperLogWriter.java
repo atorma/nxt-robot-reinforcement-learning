8de77e1d77ee2913fc338f336fbe67685e7ad84e
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-04-12 11:39:52 +0000

    BumperLogWriter stores accumulated time

--- trunk/RobotLearning/src/main/java/org/atorma/robot/simplebumper/BumperLogWriter.java
+++ trunk/RobotLearning/src/main/java/org/atorma/robot/simplebumper/BumperLogWriter.java
@@ -7,15 +7,16 @@ import org.atorma.robot.logging.CsvLogWriter;
 public class BumperLogWriter {
 
 	private CsvLogWriter logWriter;
-	private long currentTime = -1;
+	private long previousLogTime = -1;
+	private long time = 0;
 
 	public BumperLogWriter(String fileName) {
 		logWriter = new CsvLogWriter(new File(fileName), "Time ms", "Accumulated reward", "Accumulated collisions", "Collision", "Action"); 
 	}
 	
 	public void log(double accumulatedReward, int accumulatedCollisions, boolean isCollided, BumperAction action) {
-		long time = currentTime < 0 ? 0 : System.currentTimeMillis() - currentTime;
+		time += previousLogTime < 0 ? 0 : System.currentTimeMillis() - previousLogTime;
 		logWriter.addRow(time, accumulatedReward, accumulatedCollisions, isCollided ? "TRUE" : "FALSE" , action);
-		currentTime = System.currentTimeMillis();
+		previousLogTime = System.currentTimeMillis();
 	}
 }
