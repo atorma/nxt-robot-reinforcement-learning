43d9fec741d377e137b62ac1145b95cedf6f1472
anssi.torma@gmail.com <anssi.torma@gmail.com@4a924298-89ea-79e4-4be3-8ba1435a573c>
2014-04-12 11:20:54 +0000

    Fixes for running NXT bumper

--- RobotLearning/src/main/java/org/atorma/robot/simplebumper/BumperPerceptDiscretizer.java
+++ RobotLearning/src/main/java/org/atorma/robot/simplebumper/BumperPerceptDiscretizer.java
@@ -8,11 +8,7 @@ public class BumperPerceptDiscretizer implements StateDiscretizer {
 	private VectorDiscretizer idFunction;
 
 	public BumperPerceptDiscretizer() {
-		idFunction = new VectorDiscretizerImpl(new ObstacleDistanceDiscretizer(), new CollisionDiscretizer(), getLightValueDiscretizer());
-	}
-			
-	private Discretizer getLightValueDiscretizer() {
-		return new EqualWidthDiscretizer(0, 800, 5); // TODO sensible discretization
+		idFunction = new VectorDiscretizerImpl(new ObstacleDistanceDiscretizer(), new CollisionDiscretizer());
 	}
 	
 	@Override
